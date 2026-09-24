0b1ad0bd86c8f0f51c1bccf4d25ee778d792998f
chemisttasker11-ezy <chemisttasker11@gmail.com>
2026-09-24 13:29:40 +1000

    style(mobile): align legacy color constants with brand

--- frontend_mobile/constants/Colors.ts
+++ frontend_mobile/constants/Colors.ts
@@ -1,26 +1,20 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+import { brandColors } from './theme';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    text: brandColors.navy,
+    background: brandColors.mist,
+    tint: brandColors.purple,
+    icon: brandColors.body,
+    tabIconDefault: '#8A97AA',
+    tabIconSelected: brandColors.purple,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    text: '#EAF1FF',
+    background: '#08111F',
+    tint: '#A992E8',
+    icon: '#A9B8D3',
+    tabIconDefault: '#8190A6',
+    tabIconSelected: '#A992E8',
   },
 };
