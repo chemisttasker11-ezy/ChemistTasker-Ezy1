d9d0cdf9a4a473b7237fd25877c036a9ce4ade95
chemisttasker11-ezy <chemisttasker11@gmail.com>
2026-09-24 15:42:49 +1000

    feat(mobile): add learning materials route

---
+++ frontend_mobile/app/explorer/learning.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import LearningMaterialsScreen from '@/roles/shared/LearningMaterialsScreen';
+
+export default function LearningScreen() {
+  return <LearningMaterialsScreen />;
+}
