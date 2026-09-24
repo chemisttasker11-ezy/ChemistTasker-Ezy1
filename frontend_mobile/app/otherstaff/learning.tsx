089e9f80c6c457ff76a6841c4ef9477d8658b380
chemisttasker11-ezy <chemisttasker11@gmail.com>
2026-09-24 15:42:36 +1000

    refactor(mobile): use shared learning materials screen

--- frontend_mobile/app/otherstaff/learning.tsx
+++ frontend_mobile/app/otherstaff/learning.tsx
@@ -1,1 +1,6 @@
-export { default } from '@/roles/pharmacist/learning';
+import React from 'react';
+import LearningMaterialsScreen from '@/roles/shared/LearningMaterialsScreen';
+
+export default function LearningScreen() {
+  return <LearningMaterialsScreen />;
+}
